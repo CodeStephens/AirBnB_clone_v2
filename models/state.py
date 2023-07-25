8c3d67b05dbac2ffc04f0c3a82995abfc9cf0282
CodeStephens <oluwatosin.stephens@gmail.com>
2023-07-25 04:30:55 +0100

    modified state.py code

--- models/state.py
+++ models/state.py
@@ -6,3 +6,16 @@ from models.base_model import BaseModel
 class State(BaseModel):
     """ State class """
     name = ""
+
+
+class State(BaseModel):
+    @property
+    def cities(self):
+    """Return the list of City objects from storage linked to the current State."""
+        from models import storage
+        cities_list = []
+        for city in list(storage.all(City).values()):
+            if city.state_id == self.id:
+                cities_list.append(city)
+        return cities_list
+
